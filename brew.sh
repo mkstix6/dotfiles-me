e156826cfc57fb2025c89d9488b0f8ea3c658d9d
Mark Stickling <mark.stickling@minttwist.com>
2016-11-03 23:31:07 +0000

    removed and added some things to the brew

--- brew.sh
+++ brew.sh
@@ -53,32 +53,6 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
 brew install dark-mode
@@ -86,6 +60,7 @@ brew install dark-mode
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
+brew install node # This installs `npm` too using the recommended installation method
 brew install lua
 brew install lynx
 brew install p7zip
@@ -101,6 +76,8 @@ brew install vbindiff
 brew install webkit2png
 brew install zopfli
 brew install ffmpeg --with-libvpx
+
+brew install zsh
 
 # Remove outdated versions from the cellar.
 brew cleanup
