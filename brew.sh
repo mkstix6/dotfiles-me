2b3bedbcca15ef645ffccccc5209f24399c304ba
Mark Stickling <mark.stickling@minttwist.com>
2016-11-03 03:26:40 +0000

    more items for a full MacOS install

--- brew.sh
+++ brew.sh
@@ -100,6 +100,7 @@ brew install tree
 brew install vbindiff
 brew install webkit2png
 brew install zopfli
+brew install ffmpeg --with-libvpx
 
 # Remove outdated versions from the cellar.
 brew cleanup
